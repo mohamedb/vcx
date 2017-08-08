e85e6d3d66f9331c04cbeb43ecfbb7b871c29247
mohamed boullouz <mohamed.boullouz@inova-software.com>
2017-08-08 15:32:18 +0200

    bp

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Intent, Spinner,AnchorButton , Button, Dialog } from "@blueprintjs/core";
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -6,6 +7,50 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the 'undefined' type.
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <div>
+             <b>Hello from {this.props.compiler} and {this.props.framework}!</b> <br/>
+             <AnchorButton text="Click" />
+             <DialogExample />
+        </div>;
     }
+}
+
+
+
+interface IDialogExampleState {
+    isOpen: boolean;
+}
+ 
+class DialogExample extends React.Component<{}, IDialogExampleState> {
+    public state = { isOpen: false };
+ 
+    public render() {
+        return (
+            <div>
+                <Button onClick={this.toggleDialog} text="Show dialog" />
+                <Dialog
+                    iconName="inbox"
+                    isOpen={this.state.isOpen}
+                    onClose={this.toggleDialog}
+                    title="Dialog header"
+                >
+                    <div className="pt-dialog-body">
+                        Some content
+                    </div>
+                    <div className="pt-dialog-footer">
+                        <div className="pt-dialog-footer-actions">
+                            <Button text="Secondary" />
+                            <Button
+                                intent={Intent.PRIMARY}
+                                onClick={this.toggleDialog}
+                                text="Primary"
+                            />
+                        </div>
+                    </div>
+                </Dialog>
+            </div>
+        );
+    }
+ 
+    private toggleDialog = () => this.setState({ isOpen: !this.state.isOpen });
 }
